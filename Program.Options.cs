058e8fda9910f3c57e2b3e36e88f977f77a7baf9
KitStar <774968931@qq.com>
2018-05-16 00:13:24 +0800

    First

---
+++ Program.Options.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+
+namespace excel2json {
+    partial class Program {
+        /// <summary>
+        /// 命令行参数定义
+        /// </summary>
+        internal sealed class Options {
+            public Options() {
+                this.HeaderRows = 3;
+                this.Encoding = "utf8-nobom";
+                this.Lowcase = false;
+                this.ExportArray = false;
+            }
+
+            [Option('e', "excel", Required = true, HelpText = "input excel file path.")]
+            public string ExcelPath {
+                get;
+                set;
+            }
+
+            [Option('j', "json", Required = false, HelpText = "export json file path.")]
+            public string JsonPath {
+                get;
+                set;
+            }
+
+            [Option('s', "sql", Required = false, HelpText = "export SQL file path.")]
+            public string SQLPath {
+                get;
+                set;
+            }
+
+            [Option('p', "csharp", Required = false, HelpText = "export C# data struct code file path.")]
+            public string CSharpPath {
+                get;
+                set;
+            }
+
+            [Option('h', "header", Required = true, HelpText = "number lines in sheet as header.")]
+            public int HeaderRows {
+                get;
+                set;
+            }
+
+            [Option('c', "encoding", Required = false, DefaultValue = "utf8-nobom", HelpText = "export file encoding.")]
+            public string Encoding {
+                get;
+                set;
+            }
+
+            [Option('l', "lowcase", Required = false, DefaultValue = false, HelpText = "convert filed name to lowcase.")]
+            public bool Lowcase {
+                get;
+                set;
+            }
+
+            [Option('a', "array", Required = false, DefaultValue = false, HelpText = "export as array, otherwise as dict object.")]
+            public bool ExportArray {
+                get;
+                set;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+using Excel;
+
+namespace excel2json {
+    /// <summary>
+    /// 应用程序
+    /// </summary>
+    sealed partial class Program {
+        /// <summary>
+        /// 应用程序入口
+        /// </summary>
+        /// <param name="args">命令行参数</param>
+        [STAThread]
+        static void Main(string[] args) {
+            if (args.Length <= 0) {
+                //-- GUI MODE ----------------------------------------------------------
+                Console.WriteLine("Launch excel2json GUI Mode...");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new GUI.MainForm());
+            }
+            else {
+                //-- COMMAND LINE MODE -------------------------------------------------
+                
+                //-- 分析命令行参数
+                var options = new Options();
+                var parser = new CommandLine.Parser(with => with.HelpWriter = Console.Error);
+
+                if (parser.ParseArgumentsStrict(args, options, () => Environment.Exit(-1))) {
+                    //-- 执行导出操作
+                    try {
+                        DateTime startTime = DateTime.Now;
+                        Run(options);
+                        //-- 程序计时
+                        DateTime endTime = DateTime.Now;
+                        TimeSpan dur = endTime - startTime;
+                        Console.WriteLine(
+                            string.Format("[{0}]：\tConversion complete in [{1}ms].",
+                            Path.GetFileName(options.ExcelPath),
+                            dur.TotalMilliseconds)
+                            );
+                    }
+                    catch (Exception exp) {
+                        Console.WriteLine("Error: " + exp.Message);
+                    }
+                }
+            }// end of else
+        }
+
+        /// <summary>
+        /// 根据命令行参数，执行Excel数据导出工作
+        /// </summary>
+        /// <param name="options">命令行参数</param>
+        private static void Run(Options options) {
+            string excelPath = options.ExcelPath;
+            string excelName = Path.GetFileNameWithoutExtension(options.ExcelPath);
+            int header = options.HeaderRows;
+
+            // 加载Excel文件
+            using (FileStream excelFile = File.Open(excelPath, FileMode.Open, FileAccess.Read)) {
+                // Reading from a OpenXml Excel file (2007 format; *.xlsx)
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(excelFile);
+
+                // The result of each spreadsheet will be created in the result.Tables
+                excelReader.IsFirstRowAsColumnNames = true;
+                DataSet book = excelReader.AsDataSet();
+
+                // 数据检测
+                if (book.Tables.Count < 1) {
+                    throw new Exception("Excel file is empty: " + excelPath);
+                }
+
+                // 取得数据
+                DataTable sheet = book.Tables[0];
+                if (sheet.Rows.Count <= 0) {
+                    throw new Exception("Excel Sheet is empty: " + excelPath);
+                }
+
+                //-- 确定编码
+                Encoding cd = new UTF8Encoding(false);
+                if (options.Encoding != "utf8-nobom") {
+                    foreach (EncodingInfo ei in Encoding.GetEncodings()) {
+                        Encoding e = ei.GetEncoding();
+                        if (e.HeaderName == options.Encoding) {
+                            cd = e;
+                            break;
+                        }
+                    }
+                }
+
+                //-- 导出JSON文件
+                if (options.JsonPath != null && options.JsonPath.Length > 0) {
+                    JsonExporter exporter = new JsonExporter(sheet, header, options.Lowcase, options.ExportArray);
+                    exporter.SaveToFile(options.JsonPath, cd);
+                }
+
+                //-- 导出SQL文件
+                if (options.SQLPath != null && options.SQLPath.Length > 0) {
+                    SQLExporter exporter = new SQLExporter(excelName, sheet, header);
+                    exporter.SaveToFile(options.SQLPath, cd);
+                }
+
+                //-- 生成C#定义文件
+                if (options.CSharpPath != null && options.CSharpPath.Length > 0) {
+                    CSDefineGenerator exporter = new CSDefineGenerator(excelName, sheet);
+                    exporter.SaveToFile(options.CSharpPath, cd);
+                }
+            }
+        }
+    }
+}
